ce0dfb43d8c9863db12d86e8a1ac8d8c6030a483
usjuandiegot-droid <us.juandiegot@gmail.com>
2026-07-11 15:23:12 -0500

    Update procesador.py

--- procesador.py
+++ procesador.py
@@ -1,2 +1,23 @@
-def procesar_archivo():
-    return "Procesador listo"
+import os
+import shutil
+
+OUTPUT_FOLDER = "outputs"
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+
+def procesar_colombia(ruta_entrada):
+    nombre = "GLOBANT COLOMBIA Wide.xlsx"
+    ruta_salida = os.path.join(OUTPUT_FOLDER, nombre)
+
+    shutil.copy(ruta_entrada, ruta_salida)
+
+    return ruta_salida
+
+
+def procesar_peru(ruta_entrada):
+    nombre = "GLOBANT PERU Wide.xlsx"
+    ruta_salida = os.path.join(OUTPUT_FOLDER, nombre)
+
+    shutil.copy(ruta_entrada, ruta_salida)
+
+    return ruta_salida
