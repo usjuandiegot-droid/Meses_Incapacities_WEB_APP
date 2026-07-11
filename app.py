37c0e8c00d648ac96532c3bce655bcaed57b6df9
usjuandiegot-droid <us.juandiegot@gmail.com>
2026-07-11 14:29:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,18 @@ def home():
     return {
         "status": "ok",
         "mensaje": "API Globant Incapacidades Wide funcionando"
+    }
+
+@app.route("/procesar", methods=["POST"])
+def procesar():
+
+    colombia = request.files.get("colombia")
+    peru = request.files.get("peru")
+
+    return {
+        "status": "ok",
+        "archivo_colombia": colombia.filename if colombia else None,
+        "archivo_peru": peru.filename if peru else None
     }
 
 if __name__ == "__main__":
